247965f6682c9f9cb8454eabc24deaeda8c5df95
leejiwon <2jiwone@ewhain.net>
2023-07-14 06:46:50 +0900

    feat:post 정렬 구현

--- candy/post/views.py
+++ candy/post/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from post.models import Posting
+from django.db.models import Count
 
 def Post(request):
-    posts=Posting.objects.all()
-    return render(request, 'post.html', {'post':posts})
+    sort_condition=request.GET.get('sort', 'name')
+    # http://127.0.0.1:8000/countermeasure/?sort=recent
+    if sort_condition=="recent":
+        posts=Posting.objects.order_by('-upload_time')
+        return render(request, 'post.html', {'post': posts})
+    # http://127.0.0.1:8000/countermeasure/?sort=name
+    elif sort_condition=="name":
+        posts=Posting.objects.order_by('title')
+        return render(request, 'post.html', {'post': posts})
+    #posts=Posting.objects.all()
+
 
 def Post_detail(request, pk):
     post_detail=get_object_or_404(Posting, pk=pk)
